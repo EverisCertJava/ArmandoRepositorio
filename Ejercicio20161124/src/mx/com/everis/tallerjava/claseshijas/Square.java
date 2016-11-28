d7993d2a74bc4e3f93101d1cf3c4099f26e19bf3
ArmandoPerezAmaro <armando.perez.amaro@everis.com>
2016-11-28 00:08:32 -0600

    Ejercicio Fin 20161124

---
+++ Ejercicio20161124/src/mx/com/everis/tallerjava/claseshijas/Square.java
@@ -0,0 +1,12 @@
+package mx.com.everis.tallerjava.claseshijas;
+
+import mx.com.everis.tallerjava.inter.Shape;
+
+public class Square implements Shape{
+
+	@Override
+	public void draw() {
+		System.out.println("Draw Square");
+	}
+
+}
